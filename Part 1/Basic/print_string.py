4bf0fc6c998772810d5d03cadc3e65fdf65da05b
ajithajith72389 <ajithajith72389@gmail.com>
2024-09-12 18:31:31 +0530

    Basic Part

---
+++ Part 1/Basic/print_string.py
@@ -0,0 +1,9 @@
+print("Enter your String input")
+x = input()
+
+# input - Twinkle, twinkle, little star, 
+# How I wonder what you are! Up above the world so high, Like a diamond in the sky. 
+# Twinkle, twinkle, little star, How I wonder what you are
+
+print("Your input is:")
+print(x)
