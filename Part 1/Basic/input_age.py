83181fea79c0985718c6bb109ac8d2f614ca9e47
ajithajith72389 <ajithajith72389@gmail.com>
2024-09-14 11:25:06 +0530

    Age Problem

---
+++ Part 1/Basic/input_age.py
@@ -0,0 +1,9 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+name = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+
+for i in range(age,  101):
+    if age + i == 100:
+        print(f"You will turn  100 years old in the year: {2024 + i} ") 
