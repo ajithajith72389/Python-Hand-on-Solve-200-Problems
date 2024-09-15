04ea197e8ab535d151b259650e51379c8f7e0a28
ajithajith72389 <ajithajith72389@gmail.com>
2024-09-15 15:29:21 +0530

    2D Array

--- Part 1/Basic/2D_Array.py
+++ Part 1/Basic/2D_Array.py
@@ -8,3 +8,14 @@
 # Input number of columns: 4  
 # Output
 # [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6]]
+
+no_rows = int(input("Enter number of rows: "))
+no_cols = int(input("Enter number of cols: "))
+
+multi_list = [[2 for col in range(no_cols)] for row in range(no_rows)]
+
+for row in  range(no_rows):
+    for col in range(no_cols):
+        multi_list[row][col] = row * col
+
+print(multi_list)
