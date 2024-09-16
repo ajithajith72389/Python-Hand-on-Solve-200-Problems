ca2d45a7df5f40c5164b92a03c78763c31340968
ajithajith72389 <ajithajith72389@gmail.com>
2024-09-16 11:58:11 +0530

    Tri Angle

--- Part 1/Basic/check_triangle.py
+++ Part 1/Basic/check_triangle.py
@@ -1,1 +1,12 @@
 # Write a Python program to check a triangle is valid or not
+
+first = int(input("Enter the  first side of the triangle: "))
+second = int(input("Enter  the second side of the triangle: "))
+third = int(input("Enter  the third side of the triangle: "))
+
+if  (first + second >= third) or (second + third >= first) or (third + first >= second):
+    print("Yes, We can form a Tri-Angle..")
+else:
+    print("No,  We can't form a Tri-Angle..")  # Output: No, We can't form
+
+
