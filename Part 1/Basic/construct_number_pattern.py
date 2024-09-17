50e7c9efe97bf7ccb9b02eec035cde61d9d9e953
ajithajith72389 <ajithajith72389@gmail.com>
2024-09-17 10:39:24 +0530

    Pattern Printing

---
+++ Part 1/Basic/construct_number_pattern.py
@@ -0,0 +1,17 @@
+# Write a Python program to construct the following pattern, using a nested loop number.
+# 1                                                                                                             
+# 22                                                                                                            
+# 333                                                                                                           
+# 4444                                                                                                          
+# 55555                                                                                                         
+# 666666                                                                                                        
+# 7777777                                                                                                       
+# 88888888                                                                                                      
+# 999999999  
+
+num = int(input("Enter  a number: "))
+
+for  i in range(1, num + 1):
+    for j in range(1, i + 1):
+        print(i)
+
