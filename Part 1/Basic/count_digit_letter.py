3dd82fe7e43f764899d2c3da0bb054b8fe9f2c86
ajithajith72389 <ajithajith72389@gmail.com>
2024-09-21 11:07:22 +0530

    Final Answer

--- Part 1/Basic/count_digit_letter.py
+++ Part 1/Basic/count_digit_letter.py
@@ -4,3 +4,13 @@
 # Letters 6 
 # Digits 2
 
+sentence = input("Enter the sentence: ")
+digit = 0
+letter = 0
+for  char in sentence:
+    if char.isdigit():
+        digit = digit + 1
+    elif char.isalpha():
+        letter = letter + 1
+
+print("Digit: ", digit, " And letter : ", letter)  # This will print the number of digits and letters in the sentence. 
