562558b676fa82c92e75d3d802f4d86ba2a32a97
ajithajith72389 <ajithajith72389@gmail.com>
2024-10-05 10:01:47 +0530

    Sum of all the divisors

---
+++ Find_all_divisor.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+total_sum = 0
+
+for  i in range(1, num ):
+    if num % i == 0:
+        total_sum += i
+
+if  num <= 1:
+    print("False")
+elif num  == total_sum:
+    print("True")
+else:
+    print("False")
+
+
