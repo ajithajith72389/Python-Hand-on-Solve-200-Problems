abd7dc33db9f519d77262987e631ee2a640d5f2d
ajithajith72389 <ajithajith72389@gmail.com>
2024-10-05 09:58:32 +0530

    Frequency of each word in a String

--- Frequency_String.py
+++ Frequency_String.py
@@ -10,5 +10,17 @@ for element in text:
     else:
         freq[element] = 1
 
+'''
+# Sorted  by value
+
+sorted_freq = dict(sorted(freq.items(), key = lambda item:item[0]))
+
+#sorted by key
+
+sorted_freq = dict(sorted(freq.items(), key = lambda item:item[1]))
+
+
+'''
+
 for element, freq in freq.items():
     print(element, freq, end = " ")
