c99c8e0af83bd0ebe59064f7c72a302047b34f05
ajithajith72389 <ajithajith72389@gmail.com>
2024-10-08 15:36:10 +0530

    count odd and even numbers

--- Part 1/Basic/count_odd_even.py
+++ Part 1/Basic/count_odd_even.py
@@ -1,6 +1,21 @@
 # Count the number of even and odd numbers from a series of numbers
 # Input 
-# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) # Declaring the tuple
+# numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+# Declaring the tuple
 # Output
 # Number of even numbers : 4                                                                                    
 # Number of odd numbers : 5 
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+even_count = 0
+odd_count = 0
+
+for  num in numbers:
+    if num % 2 == 0:
+        even_count = even_count + 1
+    else:
+        odd_count = odd_count + 1
+
+print("Number of even numbers: ", even_count)
+print("Number of odd numbers: ", odd_count)   
