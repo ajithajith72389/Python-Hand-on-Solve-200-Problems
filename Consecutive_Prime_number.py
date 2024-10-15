3077303d98046e435684b9a2a199566165fb7335
ajithajith72389 <ajithajith72389@gmail.com>
2024-10-15 12:28:01 +0530

    Consecutive Prime number based on your wish

---
+++ Consecutive_Prime_number.py
@@ -0,0 +1,16 @@
+stop = int(input("Enter the  number of consecutive Prime Numbers: "))
+start = int(input("Enter the starting number: "))
+
+count = 0
+
+for num in range(start, start+100):
+    
+    for i in range(2, num):
+        if  (num % i) == 0:
+            break
+    else:
+        count += 1
+        if count < stop:
+            print(num, end=", ")
+        if count == stop:
+            print(num)
