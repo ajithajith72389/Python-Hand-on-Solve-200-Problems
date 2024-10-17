917f8d0aa6f7fe2a6a5ce56580257994df603874
ajithajith72389 <ajithajith72389@gmail.com>
2024-10-17 18:04:37 +0530

    Pattern

--- Part 1/Basic/construct_pattern.py
+++ Part 1/Basic/construct_pattern.py
@@ -4,8 +4,8 @@ for  i in range(1, num + 1):
     for j in range(i):
         print("* " ,end = "")
     print()
-for i in range(num, 0, -1):
+for i in range(num+1, 0, -1):
     for j in range(i):
-        print("*  " ,end = "")
+        print("* " ,end = "")
     print()    
 
