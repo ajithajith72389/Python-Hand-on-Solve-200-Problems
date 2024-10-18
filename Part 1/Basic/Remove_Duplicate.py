d062550a73dcbf62c9cd2f21d9ae5fb46bf60dc3
ajithajith72389 <ajithajith72389@gmail.com>
2024-10-18 11:46:28 +0530

    Remove duplicates from a list.

---
+++ Part 1/Basic/Remove_Duplicate.py
@@ -0,0 +1,8 @@
+# Write a Python program to remove duplicates from a list.
+# Input a = [10,20,30,20,10,50,60,40,80,50,40]
+# Output {40, 10, 80, 50, 20, 60, 30} 
+
+
+lst = [10,20,30,20,10,50,60,40,80,50,40]
+
+print(set(lst))
