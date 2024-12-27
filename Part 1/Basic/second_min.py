280c2f63238d01c43a1b0a9985e80093832fccd2
ajithajith72389 <ajithajith72389@gmail.com>
2024-12-27 09:38:38 +0530

    program to find the second smallest number in a list

--- Part 1/Basic/second_min.py
+++ Part 1/Basic/second_min.py
@@ -4,8 +4,13 @@
 # output
 # -2
 
-second_smallest = [1, 2, -8, -2, 0]
+second_smallest = [1, 2, -8, -8, -2, 0]
 
-second_smallest = sorted(second_smallest)
+#second_smallest = sorted(second_smallest)
 
+#print(second_smallest[1])
+
+
+second_smallest = list(set(second_smallest))
+second_smallest.sort()
 print(second_smallest[1])
